cedeab25160beb496a63741ee381d72d2749176a
Brian Seeders <brian.seeders@elastic.co>
2025-05-29 17:45:03 -0400

    Some fixes, and handle setting the commit sha

--- lib/pipeline-upload.sh
+++ lib/pipeline-upload.sh
@@ -20,7 +20,24 @@ fi
 # BUILDKITE_BRANCH might have "REPO_OWNER:" as a prefix (particularly for PRs), so we should remove it
 BRANCH="${BUILDKITE_BRANCH#"${REPO_OWNER}":}"
 
-GH_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/$PIPELINE_FILE?ref=${BRANCH}"
+REF="$BUILDKITE_COMMIT"
+if [[ ! "$REF" =~ ^[0-9a-f]{40}$ ]]; then
+  if [[ "$REF" == "HEAD" ]]; then
+    REF="heads/$BRANCH"
+  fi
+
+  echo "Getting sha for '$REF'..."
+  echo "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/ref/$REF"
+  REF_RESPONSE=$(curl -sSfH "Authorization: token $VAULT_GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github+json" \
+    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/ref/$REF"
+  )
+  REF=$(echo "$REF_RESPONSE" | jq -r .object.sha)
+fi
+
+buildkite-agent meta-data set buildkite:git:commit "$REF" || true
+
+GH_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/$PIPELINE_FILE?ref=${REF}"
 
 echo "Checking for pipeline file..."
 echo "$GH_URL"
