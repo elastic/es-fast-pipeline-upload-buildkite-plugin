35e40a47f9d7b9d12b4a6b231396fd19fc26123f
Brian Seeders <brian.seeders@elastic.co>
2025-05-29 17:18:38 -0400

    Initial commit for testing

---
+++ lib/pipeline-upload.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# TODO do we need to help buildkite resolve the commit?
+
+REPO="${BUILDKITE_PULL_REQUEST_REPO:-}"
+if [[ -z "$REPO" ]]; then
+  REPO="$BUILDKITE_REPO"
+fi
+
+if [[ "$REPO" =~ github\.com[:/](.+)/(.+)\.git ]]; then
+  REPO_OWNER="${BASH_REMATCH[1]}"
+  REPO_NAME="${BASH_REMATCH[2]}"
+else
+  echo "Error: Could not parse '$REPO' into owner and repo" >&2
+  exit 1
+fi
+
+# BUILDKITE_BRANCH might have "REPO_OWNER:" as a prefix (particularly for PRs), so we should remove it
+BRANCH="${BUILDKITE_BRANCH#"${REPO_OWNER}":}"
+
+GH_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/$PIPELINE_FILE?ref=${BRANCH}"
+
+echo "Checking for pipeline file..."
+echo "$GH_URL"
+
+PIPELINE_YAML=$(curl -sSfH "Authorization: token $VAULT_GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3.raw" \
+  "$GH_URL"
+)
+
+echo ""
+echo "Will upload this pipeline:"
+echo "$PIPELINE_YAML"
+echo ""
+
+echo "$PIPELINE_YAML" | buildkite-agent pipeline upload
